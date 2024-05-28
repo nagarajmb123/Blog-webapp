dcb5fad1b50737e32658da2b7def54ecadce7c17
Nagaraj <nagarajmb235@gmail.com>
2024-05-28 19:23:16 +0530

    first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { loginComponent as Login } from '../components'
+function Login() {
+  return (
+    <div className='py-8'>
+        <loginComponent />
+    </div>
+  )
+}
+
+export default Login
